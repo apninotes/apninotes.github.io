9aba4530e91b34b08d600877fef9722fb8926791
apninotes <info@apninotes.com>
2025-12-06 20:12:31 +0530

    Change header background color class to primary-indigo

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function getCurrentYear() {
 // --- RENDERERS ---
 function renderHeader() {
     return `
-        <header class="shadow-lg bg-purple-700 text-white">
+        <header class="shadow-lg bg-primary-indigo text-white">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
                 <div class="flex items-center justify-between">
                     
